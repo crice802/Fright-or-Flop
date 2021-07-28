baf51a63fe17aaab591969f632f2580fd8921685
crice802 <rcrice@gmail.com>
2021-07-28 14:56:07 -0400

    update index for recent activity

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,35 @@
+import { Profile } from "../models/profile.js"
+import { Movie } from "../models/movie.js"
+import { Review } from '../models/review.js'
+ 
 export {
   index,
 }
 
 function index(req, res) {
-  res.render('index', {
-    title: 'Fright or Flop',
-    user: req.user ? req.user : null 
-  })
-} 
+  Movie.find({})
+  .sort({_id: -1})
+  .limit(6)
+  .populate('collectedBy')
+  .then(movies => {
+    Profile.find({})
+    .sort({_id: -1})
+    .limit(5)
+    .then(profiles => {
+      Review.find({})
+      .sort({_id: -1})
+      .limit(6)
+      .populate('movie')
+      .populate('author')
+      .then(reviews => {
+          res.render('index', {
+            title: 'Latest Activity',
+            movies,
+            profiles,
+            reviews,
+          })
+        })
+      })
+    })
+  }
+
