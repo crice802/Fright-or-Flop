21db13ee03a2308aeaa71051af6a0ce6a7f7a0d0
crice802 <rcrice@gmail.com>
2021-07-29 07:47:29 -0400

    redepoly to Heroku

--- controllers/movies.js
+++ controllers/movies.js
@@ -13,12 +13,12 @@ export {
 }
 
 function addToCollection(req, res) {
-  // Add id of the logged in user to req.body for creating a game for the first time (if it doesn't exist in the database)
+  // Add id of the logged in user to req.body for creating a movie for the first time (if it doesn't exist in the database)
   req.body.collectedBy = req.user.profile._id
-    // Look to see if the game already exists in the database
+    // Look to see if the movie already exists in the database
   Movie.findOne({ movId: req.params.id })
   .then((movie) => {
-    // If it does, push the user's profile id to game.collectedBy
+    // If it does, push the user's profile id to movie.collectedBy
     if (movie) {
       movie.collectedBy.push(req.user.profile._id)
       movie.save()
@@ -71,7 +71,6 @@ function show(req, res) {
       }
     })
     .then((movie) => {
-      console.log(movie)
       res.render("movies/show", {
         title: "Movie Details",
         apiResult: response.data,
@@ -88,7 +87,6 @@ function show(req, res) {
 function search(req, res){
   axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${ process.env.API_KEY }&query=${ req.body.search }&page=1`)
   .then(response =>{
-    console.log('hey look here',response.data.results.filter(movie => movie.genre_ids.includes(27)))
     let filteredMovies = response.data.results.filter(movie => movie.genre_ids.includes(27))
     res.render('movies/new',{
       title: 'Search Results',
