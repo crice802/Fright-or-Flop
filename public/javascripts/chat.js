32d5cf4dcc9d4001bb042db4b1d64871d94d4686
crice802 <rcrice@gmail.com>
2021-10-19 16:52:13 -0400

    Chat room loaded

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -12,44 +12,105 @@ let chatters = document.getElementById("chatters");
 
 /**
  * When 'send message' is clicked, emit a message containing the chat 
- * info to the server
+ * info to the server and send the message to the database for storage
  */
+ send_message.addEventListener("click", () => {
+  socket.emit("send_message", {
+    username: username.value,
+    message: message.value,
+    avatar: avatar.value,
+  });
+  fetch("/chatroom", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      avatar: avatar.value,
+      username: username.value,
+      message: message.value,
+    })
+  })
+  message.value = "";
+})
 
 /**
  * When a user presses the 'Enter' key, emit a message containing the 
- * chat info to the server
+ * chat info to the server and send the message to the database for storage
  */
+ message.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    socket.emit("send_message", {
+      username: username.value,
+      message: message.value,
+      avatar: avatar.value,
+    })
+    fetch("/chatroom", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        avatar: avatar.value,
+        username: username.value,
+        message: message.value,
+      })
+    })
+    message.value = "";
+  }
+})
 
 /**
  * When a user presses a key while typing in the 'message' element, 
  * send the user's name to the server
  */
+message.addEventListener("keypress", () => {
+  socket.emit("typing", { username: username.value });
+});
 
 
 //-----*  Socket Events Here:  *-----// 
 
 /**
- * Define/execute a function to get the username from the server so 
- * that it can be broadcast on connection
+ * Send the username to register them with the server 
+ * 
  */
+function registerUser() {
+  socket.emit("register-user", username.value)
+}
+
+registerUser()
 
 /**
  * When the socket receives an updated chat list, re-render the list 
  * of users connected
  */
+socket.on('update-chatter-list', username => {
+  let chatterList = "<li>" + username.join("</li><li>") + "</li>";
+  chatters.innerHTML = chatterList;
+})
 
 // When a user enters the room, play a sound
-
+socket.on('user-enter', ()=> {
+  enterAudio.play()
+})
 // When a user leaves the room, play a sound
-
+socket.on('user-exit', ()=> {
+  exitAudio.play()
+})
 /**
  * When someone is typing (something we'll need the server to tell us),
  * adjust the 'isTyping' element to reflect that
  */
+socket.on("typing", (data) => {
+  isTyping.innerText = `${data.username} is typing...`;
+});
 
 /**
  * When a new message is posted, play a sound, update the newMessage 
- * element with the message/user info, and add the message to the 
- * database (we'll check server-side to make sure the message is only 
- * posted once, by checking the id of the user making the post)
+ * element with the message/user info.
  */
+
+ socket.on("new_message", (data) => {
+  isTyping.innerText = "";
+  messageAudio.play();
+  let newMessage = document.createElement("p");
+  newMessage.innerHTML = `<p><img id="avatar" height="30" src="${data.avatar}" alt=""> ${data.username}: ${data.message}</p>`;
+  chatroom.prepend(newMessage);
+});
